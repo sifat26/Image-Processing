d2afbdf68a4789b60354c541866ec736731e1a6c
sifat26 <sifatict26@gmail.com>
2024-04-23 00:52:23 +0600

    first commit

---
+++ program1.py
@@ -0,0 +1,9 @@
+import cv2
+img1=cv2.imread("image/kids.tif",cv2.IMREAD_COLOR)
+img2=cv2.imread("image/kids.tif",cv2.IMREAD_GRAYSCALE)
+img3=cv2.imread("image/kids.tif",1)
+img4=cv2.imread("image/kids.tif",2)
+
+cv2.imshow("Input Image",img3)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
