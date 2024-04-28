020b4ddad2fe15de9e0089494d2e1f63b7b7d13e
sifat26 <sifatict26@gmail.com>
2024-04-28 12:04:51 +0600

    Image Thresholding added

--- Histogram of an image.py
+++ Histogram of an image.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
-img=cv.imread('image/cameraman.tif',0)
+img=cv.imread('image/kids.tif',0)
 cv.imshow("Image",img)
 plt.hist(img.ravel(),256,[0,256])
 plt.show()
---
+++ Image Thresoldinh.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+image=cv2.imread('image/lena_color.tiff',cv2.IMREAD_GRAYSCALE)
+threshold_value=128
+max_value=255
+_, thresholded_image=cv2.threshold(image,threshold_value,max_value,cv2.THRESH_BINARY)
+cv2.imshow('Original Image',image)
+cv2.imshow('Thresholded Image',thresholded_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
