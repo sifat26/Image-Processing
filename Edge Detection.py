48c49421d863d0bbd00b9edf07b52d50edb5f3fd
sifat26 <sifatict26@gmail.com>
2024-05-11 01:23:57 +0600

    File update

--- Edge Detection.py
+++ Edge Detection.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-image=cv2.imread("image/kids.tif")
+image=cv2.imread("image/lena_color.tiff")
 gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 blurred=cv2.GaussianBlur(gray,(5,5),0)
 edge=cv2.Canny(blurred,50,150)
