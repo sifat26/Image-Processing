78d1b7004ee68b4e2e5f38db424a51e9bb08a034
sifat26 <sifatict26@gmail.com>
2024-05-29 02:22:10 +0600

    Updated

--- Z-score Normalization.py
+++ Z-score Normalization.py
@@ -1,5 +1,5 @@
 import cv2
-image=cv2.imread("image/5.jpg",cv2.IMREAD_COLOR)
+image=cv2.imread("Viral Pneumonia/19.jpg",cv2.IMREAD_COLOR)
 normalized_image = (image - image.mean()) / image.std()
 
 cv2.imshow("Resized Image",normalized_image)
