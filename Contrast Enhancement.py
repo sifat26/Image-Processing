6c07a6b3c2271754df7953dc13a248a466cf927e
sifat26 <sifatict26@gmail.com>
2024-05-30 02:29:51 +0600

    File update

--- Contrast Enhancement.py
+++ Contrast Enhancement.py
@@ -1,5 +1,5 @@
 import cv2
-gray_image=cv2.imread('image/5.jpg',cv2.IMREAD_GRAYSCALE)
+gray_image=cv2.imread('Viral Pneumonia/18.jpg',cv2.IMREAD_GRAYSCALE)
 equalized_image=cv2.equalizeHist(gray_image)
 cv2.imshow("Original Image",gray_image)
 cv2.imshow("Equalized Image",equalized_image)
