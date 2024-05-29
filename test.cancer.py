16163efe9f5ef87ba6b8eb3fc4a2156178369c79
sifat26 <sifatict26@gmail.com>
2024-05-30 02:35:37 +0600

    File update

--- test.cancer.py
+++ test.cancer.py
@@ -1,64 +1,17 @@
-import os
-import numpy as np
 import cv2
-from sklearn.model_selection import train_test_split
-import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from sklearn.metrics import classification_report, confusion_matrix
+import lungc
+import pickle
 
-# Function to load and preprocess images
-def load_images_from_folder(folder):
-    images = []
-    labels = []
-    for filename in os.listdir(folder):
-        img_path = os.path.join(folder, filename)
-        img = cv2.imread(img_path)
-        if img is not None:
-            img = cv2.resize(img, (128, 128))  # Resize to 128x128
-            images.append(img)
-            label = 1 if 'cancer' in filename.lower() else 0  # Assuming filenames indicate the label
-            labels.append(label)
-    return np.array(images), np.array(labels)
+num=input("Enter test image number: ")
+img=cv2.imread("test/test ("+num+").jpg",0)
+left_feature,right_feature,img_left,img_right=lungc.process_lung_test(img)
 
-# Load the data
-folder_path = r'C:\Users\sifat\PycharmProjects\opencv1\Viral Pneumonia'
-images, labels = load_images_from_folder(folder_path)
 
-# Normalize pixel values
-images = images / 255.0
-
-# Split the data
-X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
-
-# Define the model
-model = Sequential([
-    Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 3)),
-    MaxPooling2D((2, 2)),
-    Conv2D(64, (3, 3), activation='relu'),
-    MaxPooling2D((2, 2)),
-    Conv2D(128, (3, 3), activation='relu'),
-    MaxPooling2D((2, 2)),
-    Flatten(),
-    Dense(128, activation='relu'),
-    Dropout(0.5),
-    Dense(1, activation='sigmoid')
-])
-
-# Compile the model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-# Train the model
-history = model.fit(X_train, y_train, epochs=10, validation_split=0.2)
-
-# Evaluate the model
-loss, accuracy = model.evaluate(X_test, y_test)
-print(f'Test accuracy: {accuracy:.2f}')
-
-# Make predictions
-predictions = model.predict(X_test)
-predictions = (predictions > 0.5).astype(int)
-
-# Metrics
-print(classification_report(y_test, predictions))
-print(confusion_matrix(y_test, predictions))
+filename = 'finalized_model.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+result = loaded_model.predict([left_feature])
+print(result[0])
+if(result[0]):
+	print("Tumour present")
+else:
+	print("Normal Lung")
